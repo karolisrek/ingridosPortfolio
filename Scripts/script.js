289c36e03589cd73f886007fd8bb106bb330e7f7
Karolis Rekasius <karolis.rek@gmail.com>
2020-06-10 23:29:30 +0300

    some changes on web size responsiveness

---
+++ Scripts/script.js
@@ -0,0 +1,14 @@
+function ChangeMenuState() {
+    const menuElem = document.getElementById("header");
+    const btnElem = document.getElementById("burger_menu_btn");
+
+    if (menuElem.classList.contains("open")) {
+        menuElem.classList.remove("open");
+        btnElem.classList.add("open");
+        btnElem.classList.remove("close");
+    } else {
+        menuElem.classList.add("open");
+        btnElem.classList.remove("open");
+        btnElem.classList.add("close");
+    }
+}
